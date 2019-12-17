2bed3eaa93f47876f99c320d752f1d0d65f511f4
Philipp Schrader <philipp.schrader@gmail.com>
2019-12-17 08:07:15 -0800

    Work on some screen layout logic

--- parser.cc
+++ parser.cc
@@ -1,10 +1,46 @@
 #include "parser.h"
 
+#include <algorithm>
+
 namespace ttyrex::parser {
 
 Iterator::Iterator(int row_index, const ParsedScreen& parsed_screen)
     : row_index_(row_index), parsed_screen_(&parsed_screen) {}
 
-ParsedRow Iterator::operator*() const { return parsed_screen_->GetRow(row_index_); }
+ParsedRow Iterator::operator*() const {
+  return parsed_screen_->GetRow(row_index_);
+}
+
+void ScrollOffHandler::StartNewRow() {
+  int num_columns = slots_.size() % num_rows_;
+  std::fill_n(slots_.begin(), num_columns, 0);
+  starting_row_ = (starting_row_ + 1) % num_rows_;
+}
+
+ParsedRow ParsedScreen::GetRow(int row_index) const {
+  int num_columns = slots_.size() % num_rows_;
+  return std::span(&slots_[row_index * num_columns], num_columns);
+}
+
+Parser::Parser(int read_fd, epoll::EPoll* epoll)
+    : read_fd_(read_fd), epoll_(epoll), writer_(buffer_.MakeWriter()) {
+  epoll_->Add(read_fd_, EPOLLIN | EPOLLHUP, [this] { HandleNewCharacters(); });
+}
+
+Parser::~Parser() { epoll_->Delete(read_fd_); }
+
+void Parser::HandleNewCharacters() {
+  while (true) {
+    std::span<uint8_t> buffer = writer_->Get(512);
+    ssize_t result = read(read_fd_, buffer.data(), buffer.size());
+    if (result == -1) {
+      if (result == EAGAIN || result == EWOULDBLOCK) {
+        return;
+      }
+      PCHECK(errno == EINTR) << ": Failed to read from read_fd_";
+    }
+    writer_->Rewind(buffer.size() - result);
+  }
+}
 
 }  // namespace ttyrex::parser
--- parser.h
+++ parser.h
@@ -31,7 +31,7 @@ using ParsedRow = std::span<const Slot>;
 
 class ParsedScreen;
 
-class Iterator {
+class RowIterator {
  public:
   using difference_type = int;
   using value_type = ParsedRow;
@@ -39,14 +39,14 @@ class Iterator {
   using reference = ParsedRow;
   using iterator_category = std::forward_iterator_tag;
 
-  Iterator(int row_index, const ParsedScreen &parsed_screen);
-  Iterator(const Iterator &) = default;
+  RowIterator(int row_index, const ParsedScreen &parsed_screen);
+  RowIterator(const RowIterator &) = default;
 
-  Iterator &operator=(const Iterator &) = default;
-  auto operator<=>(const Iterator &other) {
+  RowIterator &operator=(const RowIterator &) = default;
+  auto operator<=>(const RowIterator &other) {
     return row_index_ - other.row_index_;
   }
-  Iterator &operator++() {
+  RowIterator &operator++() {
     ++row_index_;
     return *this;
   }
@@ -57,13 +57,27 @@ class Iterator {
   const ParsedScreen *parsed_screen_;
 };
 
-class ParsedScreen {
+class ScrollOffHandler {
  public:
-  void Resize(int rows, int columns);
-
-  ParsedRow GetRow(int row_index) const;
+   void StartNewRow();
 
  private:
+  int num_rows_;
+  int starting_row_;
+  std::vector<Slot> slots_;
+};
+
+class ParsedScreen {
+ public:
+  ParsedRow GetRow(int row_index) const;
+
+  RowIterator begin() { return RowIterator(0, *this); }
+  RowIterator end() { return RowIterator(num_rows_, *this); }
+  RowIterator cbegin() const { return RowIterator(0, *this); }
+  RowIterator cend() const { return RowIterator(num_rows_, *this); }
+
+ private:
+  int num_rows_;
   std::vector<Slot> slots_;
 };
 
@@ -72,12 +86,17 @@ class Parser {
   Parser(int read_fd, epoll::EPoll *epoll);
   ~Parser();
 
+  void Resize(int rows, int columns);
+
   const ParsedScreen &screen() { return parsed_screen_; }
 
  private:
+  void HandleNewCharacters();
+
   const int read_fd_;
   epoll::EPoll *const epoll_;
   zero_copy::Buffer buffer_;
+  std::unique_ptr<zero_copy::Writer> writer_;
   ParsedScreen parsed_screen_;
 };
 
